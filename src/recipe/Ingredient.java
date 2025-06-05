c8469159337403c7ddd331dfd900c8e08cde6542
hozlucas28 <hozlucas28@gmail.com>
2025-06-05 18:05:17 -0300

    chore(Ingredient): add throw exception and complete tests

--- src/recipe/Ingredient.java
+++ src/recipe/Ingredient.java
@@ -1,12 +1,19 @@
 package recipe;
 
+import exceptions.OutOfRangeException;
 import inventory.Item;
 
 public class Ingredient {
 	private final Item item;
 	private final int quantity;
 
-	public Ingredient(Item item, int quantity) {
+	public Ingredient(Item item, int quantity) throws OutOfRangeException {
+		if (quantity < 1) {
+			String errorMessage = String.format("Received %d as quantity, but expect it greather than or equal to %d",
+					quantity, 1);
+			throw new OutOfRangeException(errorMessage);
+		}
+
 		this.item = item;
 		this.quantity = quantity;
 	}
--- test/recipe/IngredientTests.java
+++ test/recipe/IngredientTests.java
@@ -4,14 +4,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import exceptions.OutOfRangeException;
+import inventory.Item;
+
 class IngredientTests {
 
-    // TODO: test getItem()
-    // TODO: test getQuantity()
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getItem() {
+		assertDoesNotThrow(() -> {
+			// Arrange
+			Item item = new Item("A", "My item", null);
+			int itemQuantity = 2;
+
+			Ingredient ingredient = new Ingredient(item, itemQuantity);
+
+			// Act
+			Item expected = item;
+			Item received = ingredient.getItem();
+
+			// Assert
+			assertEquals(expected, received);
+		});
 	}
 
+	@Test
+	void getItem_OutOfRangeException() {
+		// Arrange
+		Item item = new Item("A", "My item", null);
+		int itemQuantity = 0;
+
+		// Act within assert
+		assertThrows(OutOfRangeException.class, () -> new Ingredient(item, itemQuantity));
+	}
+
+	@Test
+	void getQuantity() {
+		assertDoesNotThrow(() -> {
+			// Arrange
+			Item item = new Item("A", "My item", null);
+			int itemQuantity = 5;
+
+			Ingredient ingredient = new Ingredient(item, itemQuantity);
+
+			// Act
+			int expected = itemQuantity;
+			int received = ingredient.getQuantity();
+
+			// Assert
+			assertEquals(expected, received);
+		});
+	}
 }
