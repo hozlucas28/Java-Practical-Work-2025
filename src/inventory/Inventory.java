a6befc345d25f7b5645641d31987e694ca302e3e
hozlucas28 <hozlucas28@gmail.com>
2025-06-05 16:23:19 -0300

    feature(Inventory): develop methods logic

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -2,6 +2,9 @@ package inventory;
 
 import java.util.HashMap;
 
+import exceptions.ItemNotFoundException;
+import exceptions.OutOfRangeException;
+
 public class Inventory {
 	private HashMap<Item, Integer> items;
 
@@ -14,15 +17,46 @@ public class Inventory {
 	}
 
 	public int getItemQuantity(Item item) {
-		return this.items.get(item);
+		return this.items.getOrDefault(item, 0);
 	}
 
-	public void addItem(Item item, Integer quantity) {
-		this.items.put(item, quantity);
+	public void addItem(Item item, int quantity) throws OutOfRangeException {
+		if (quantity < 1) {
+			String errorMessage = String.format("Received %d as quantity, but expect it greather than or equal to %d",
+					quantity, 1);
+			throw new OutOfRangeException(errorMessage);
+		}
+
+		if (this.items.containsKey(item)) {
+			this.items.compute(item, (key, value) -> value + quantity);
+		} else {
+			this.items.put(item, quantity);
+		}
 	}
 
-	public void removeItem(Item item, Integer quantity) {
-		// TODO
+	public void removeItem(Item item, int quantity) throws ItemNotFoundException, OutOfRangeException {
+		String errorMessage;
+
+		if (!this.items.containsKey(item)) {
+			errorMessage = String.format("Item with `%s` id was not found inside the inventory", item.getId());
+			throw new ItemNotFoundException(errorMessage);
+		}
+
+		int currentQuantity = this.items.get(item);
+
+		if (quantity < 1 || quantity > currentQuantity) {
+			errorMessage = String.format("Received %d as quantity, but expect it between %d and %d (inclusive)",
+					quantity, 1, currentQuantity);
+			throw new OutOfRangeException(errorMessage);
+		}
+
+		int newQuantity = currentQuantity - quantity;
+
+		if (newQuantity == 0) {
+			this.items.remove(item);
+		} else {
+			this.items.replace(item, newQuantity);
+		}
 	}
 
 	public void storeOnJSON(String path) {
--- test/inventory/InventoryTests.java
+++ test/inventory/InventoryTests.java
@@ -2,20 +2,165 @@ package inventory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
+
+import exceptions.ItemNotFoundException;
+import exceptions.OutOfRangeException;
 
 class InventoryTests {
 
-	// TODO: test getItems()
-	// TODO: test getItemQuantity()
-	// TODO: test addItem()
-	// TODO: test removeItem()
-	// TODO: test storeOnJSON()
-	// TODO: test loadFromJSON()
+	@Test
+	void getItems() {
+		// Arrange
+		Item item01 = new Item("A01", "My item 01", null);
+		Item item02 = new Item("A02", "My item 02", null);
+
+		int item01Quantity = 1;
+		int item02Quantity = 3;
+
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+
+		items.put(item01, item01Quantity);
+		items.put(item02, item02Quantity);
+
+		Inventory inventory = new Inventory(items);
+
+		// Act
+		int expected = items.size();
+		int received = inventory.getItems().size();
+
+		// Assert
+		assertEquals(expected, received);
+	}
 
 	@Test
-	void test() {
+	void getItemQuantity() {
+		// Arrange
+		Item item = new Item("A", "My item", null);
+
+		int itemQuantity = 7;
+
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+
+		items.put(item, itemQuantity);
+
+		Inventory inventory = new Inventory(items);
+
+		// Act
+		int expected = itemQuantity;
+		int received = inventory.getItemQuantity(item);
+
+		// Assert
+		assertEquals(expected, received);
+	}
+
+	@Test
+	void addItem() {
+		// Arrange
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+		Inventory inventory = new Inventory(items);
+
+		Item item = new Item("A", "My item", null);
+
+		int itemQuantity = 7;
+
+		assertDoesNotThrow(() -> {
+			// Act
+			inventory.addItem(item, itemQuantity);
+
+			int expected = itemQuantity;
+			int received = inventory.getItemQuantity(item);
+
+			// Assert
+			assertEquals(expected, received);
+		});
+	}
+
+	void addItem_OutOfRangeException() {
+		// Arrange
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+		Inventory inventory = new Inventory(items);
+
+		Item item = new Item("A", "My item", null);
+
+		int itemQuantity = 0;
+
+		// Act within assert
+		assertThrows(OutOfRangeException.class, () -> inventory.addItem(item, itemQuantity));
+	}
+
+	@Test
+	void removeItem() {
+		// Arrange
+		Item item = new Item("A", "My item", null);
+
+		int itemQuantity = 7;
+
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+
+		items.put(item, itemQuantity);
+
+		Inventory inventory = new Inventory(items);
+
+		assertDoesNotThrow(() -> {
+			// Act
+			int quantityToRemove = 1;
+			inventory.removeItem(item, quantityToRemove);
+
+			int expected = itemQuantity - quantityToRemove;
+			int received = inventory.getItemQuantity(item);
+
+			// Assert
+			assertEquals(expected, received);
+		});
+	}
+
+	void removeItem_ItemNotFoundException() {
+		// Arrange
+		Item item = new Item("A", "My item", null);
+
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+		Inventory inventory = new Inventory(items);
+
+		// Assert
+		assertThrows(ItemNotFoundException.class, () -> {
+			// Act
+			int quantityToRemove = 1;
+			inventory.removeItem(item, quantityToRemove);
+		});
+	}
+
+	void removeItem_OutOfRangeException() {
+		// Arrange
+		Item item = new Item("A", "My item", null);
+
+		int itemQuantity = 7;
+
+		HashMap<Item, Integer> items = new HashMap<Item, Integer>();
+
+		items.put(item, itemQuantity);
+
+		Inventory inventory = new Inventory(items);
+
+		// Assert
+		assertThrows(OutOfRangeException.class,() -> {
+			// Act
+			int quantityToRemove = 10;
+			inventory.removeItem(item, quantityToRemove);
+		});
+	}
+
+	@Test
+	void storeOnJSON() {
+		// TODO
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void loadFromJSON() {
+		// TODO
+		fail("Not yet implemented");
+	}
 }
