0bcfac955fe7a3c9d14943d38abc1ff33aad1b49
hozlucas28 <hozlucas28@gmail.com>
2025-06-09 22:08:43 -0300

    feature(CraftedItem): add `craftedItems` attribute and getter

--- src/craftingSystem/CraftedItem.java
+++ src/craftingSystem/CraftedItem.java
@@ -3,17 +3,35 @@ package craftingSystem;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import exceptions.OutOfRangeException;
 import inventory.Item;
 import recipe.Recipe;
 
 public class CraftedItem extends Item {
 	private final ZonedDateTime date;
 	private final Recipe usedRecipe;
+	private final int craftedItems;
 
 	public CraftedItem(String id, String name, List<Recipe> recipes, Recipe usedRecipe) {
 		super(id, name, recipes);
 		this.date = ZonedDateTime.now();
 		this.usedRecipe = usedRecipe;
+		this.craftedItems = 1;
+	}
+
+	public CraftedItem(String id, String name, List<Recipe> recipes, Recipe usedRecipe, int craftedItems)
+			throws OutOfRangeException {
+		super(id, name, recipes);
+
+		if (craftedItems < 1) {
+			String errorMessage = String.format("Received %d as craftedItems, but expect it greater than or equal to %d",
+					craftedItems, 1);
+			throw new OutOfRangeException(errorMessage);
+		}
+
+		this.date = ZonedDateTime.now();
+		this.usedRecipe = usedRecipe;
+		this.craftedItems = craftedItems;
 	}
 
 	public ZonedDateTime getDate() {
@@ -22,5 +40,9 @@ public class CraftedItem extends Item {
 
 	public Recipe getUsedRecipe() {
 		return this.usedRecipe;
+	}
+
+	public int getCraftedItems() {
+		return this.craftedItems;
 	}
 }
