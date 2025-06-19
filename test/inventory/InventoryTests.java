5205543bed9788675b21e4840c3af10367fbacc3
hozlucas28 <hozlucas28@gmail.com>
2025-06-19 03:56:18 -0300

    chore: remove unused imports

--- test/inventory/InventoryTests.java
+++ test/inventory/InventoryTests.java
@@ -3,7 +3,6 @@ package inventory;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
