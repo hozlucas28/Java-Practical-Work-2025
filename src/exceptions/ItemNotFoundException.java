dc95ad80537dd203169fc4a238b4123c08cea4eb
hozlucas28 <hozlucas28@gmail.com>
2025-06-25 00:06:53 -0300

    chore(Exceptions): add `serialVersionUID` attributes

--- src/exceptions/ItemNotFoundException.java
+++ src/exceptions/ItemNotFoundException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class ItemNotFoundException extends Exception {
+	private static final long serialVersionUID = -7708452809775504558L;
 
 	public ItemNotFoundException(String message) {
 		super(message);
--- src/exceptions/OutOfRangeException.java
+++ src/exceptions/OutOfRangeException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class OutOfRangeException extends Exception {
+	private static final long serialVersionUID = -1018605717502691292L;
 
 	public OutOfRangeException(String message) {
 		super(message);
