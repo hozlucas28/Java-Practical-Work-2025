2793451c97321f9dee9c05d0416270a9b970c0f7
hozlucas28 <hozlucas28@gmail.com>
2025-06-28 19:49:38 -0300

    feature(StringTransformers): add `toCapitalize` method

--- src/utilities/StringTransformers.java
+++ src/utilities/StringTransformers.java
@@ -20,4 +20,11 @@ public class StringTransformers {
 		return builder.toString().trim();
 	}
 
+	public static String toCapitalize(String str) {
+		if (str == null || str.isEmpty()) {
+			return str;
+		}
+
+		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+	}
 }
