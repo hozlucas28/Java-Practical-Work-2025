766ad94b771a6671376cb75234c11f541a09bb5e
hozlucas28 <hozlucas28@gmail.com>
2025-06-29 03:13:20 -0300

    feature(CraftedItem): add `toString` method

--- src/craftingSystem/CraftedItem.java
+++ src/craftingSystem/CraftedItem.java
@@ -1,10 +1,16 @@
 package craftingSystem;
 
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
 import java.util.Objects;
 
 import inventory.Item;
+import recipe.Ingredient;
 import recipe.Recipe;
+import utilities.StringTransformers;
 
 public class CraftedItem extends Item {
 	private final ZonedDateTime date;
@@ -55,5 +61,44 @@ public class CraftedItem extends Item {
 		CraftedItem other = (CraftedItem) obj;
 
 		return Objects.equals(this.usedRecipe, other.usedRecipe) && this.quantityCrafted == other.quantityCrafted;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		Formatter formatter = new Formatter(builder);
+
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' HH:mm:ss");
+		formatter.format("%s (%s):\n", StringTransformers.toTitle(this.name), this.date.format(dateFormatter));
+
+		int recipeNumber = this.recipes.indexOf(usedRecipe) + 1;
+		formatter.format("  ◦ Recipe #%d (x%d): ", recipeNumber, this.quantityCrafted);
+
+		List<Ingredient> usedIngredients = new ArrayList<Ingredient>();
+
+		if (this.usedRecipe.needsCraftingTable()) {
+			Ingredient craftingTable = new Ingredient(this.usedRecipe.getCraftingTable(), 1);
+			usedIngredients.add(craftingTable);
+		}
+
+		usedIngredients.addAll(usedRecipe.getIngredients());
+
+		for (int i = 0; i < usedIngredients.size(); i++) {
+			Ingredient ingredient = usedIngredients.get(i);
+
+			String ingredientName = StringTransformers.toTitle(ingredient.getItem().getName());
+			int ingredientQuantity = ingredient.getQuantity();
+
+			if (i == usedIngredients.size() - 1) {
+				formatter.format("%s%s (x%d)", i == 0 ? "" : "and ", ingredientName, ingredientQuantity);
+			} else {
+				formatter.format("%s (x%d), ", ingredientName, ingredientQuantity);
+			}
+		}
+
+		formatter.format(".");
+		formatter.close();
+
+		return builder.toString();
 	}
 }
